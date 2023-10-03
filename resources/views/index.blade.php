3ab2215142f9f44eaccc6088d9c92076ef536491
Kevin <vinxek@me.com>
2023-10-03 17:09:37 -0500

    created the homepage with search bar and search all products.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,25 @@
 <x-app title="Mercadolibre | Home">
-    <h1>Hello, World!</h1>
+
+
+
+    <section class="d-flex flex-wrap justify-content-center">
+        @foreach ($products as $product)
+            <div class="card mx-2 my-3 card_size">
+                <div class="card-header">
+                    <h2 class="h5">{{ $product->product_name }}</h2>
+                </div>
+                <div class="card-body">
+                    <p>{{ $product->description }}</p>
+                </div>
+                <div class="card-footer d-flex justify-content-between align-items-center">
+                    <div class=""><button class="btn btn-primary">Add to cart</button></div>
+                    <div class="">
+                        <p><strong>${{ $product->price }}</strong></p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </section>
+
+
 </x-app>
