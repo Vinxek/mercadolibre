5f25f409809c487719178c92d301307f5f98d691
Kevin <vinxek@me.com>
2023-10-11 04:59:13 -0500

    missing category and searchbar and schema for userForm Component

--- resources/js/components/Categories/ProductCategory.vue
+++ resources/js/components/Categories/ProductCategory.vue
@@ -1,15 +1,30 @@
-<template></template>
+<template>
+	<section class="d-flex flex-wrap justify-content-center">
+
+		<div class="card d-flex card mx-2 my-3 card_size flex-wrap justify-content-center" v-for="product in products_data"
+			:key="product.id">
+			<img :src="product.file.route" class="card-img-top" alt="product image">
+			<div class="card-body">
+				<h5 class="card-title">{{ product.product_name }}</h5>
+				<p class="card-text">{{ product.description }}</p>
+				<p class="card-text">Price: ${{ product.price }}</p>
+				<p class="card-text">Stock: {{ product.stock }}</p>
+			</div>
+		</div>
+
+	</section>
+</template>
 
 <script>
-import axios from 'axios';
+
 export default {
-	props: ['products'],
+	props: ['products_data'],
 	data() {
 
 	},
 
 	mounted() {
-		console.log("debu", this.products)
+		console.log("debug", this.products_data)
 	},
 
 }
